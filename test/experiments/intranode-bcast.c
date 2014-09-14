0f471b4d0b89f9dfb70036864da32c62a3cd9cd0
Jeff Hammond <jeff.science@gmail.com>
2014-09-14 16:32:00 -0700

    fix bug with shm win usage

--- test/experiments/intranode-bcast.c
+++ test/experiments/intranode-bcast.c
@@ -18,6 +18,10 @@ int SMP_Bcast(void* buffer, int count, MPI_Datatype datatype, int root, MPI_Comm
     void * temp = NULL;
     MPI_Win wintemp = MPI_WIN_NULL;
     MPI_Win_allocate_shared(winsize, ts, MPI_INFO_NULL, MPI_COMM_NODE, &temp, &wintemp);
+
+    int disp; /* unused */
+    MPI_Win_shared_query(wintemp, nrank, &winsize, &disp, &temp);
+
     MPI_Win_lock_all(0, wintemp);
 
     if (nrank==0) {
@@ -55,17 +59,21 @@ int main(int argc, char* argv[])
     double t0, t1, dt;
 
     for (int r=0; r<20; r++) {
+        MPI_Barrier(MPI_COMM_WORLD);
         t0 = MPI_Wtime();
         MPI_Bcast(buf1, n, MPI_CHAR, 0, MPI_COMM_NODE);
         t1 = MPI_Wtime();
         dt = t1-t0;
-        printf("MPI_Bcast: %lf seconds, %lf MB/s \n", dt, n*(1.e-6/dt));
+        printf("%d: MPI_Bcast: %lf seconds, %lf MB/s \n", wrank, dt, n*(1.e-6/dt));
+        fflush(stdout);
 
+        MPI_Barrier(MPI_COMM_WORLD);
         t0 = MPI_Wtime();
         SMP_Bcast(buf2, n, MPI_CHAR, 0, MPI_COMM_NODE);
         t1 = MPI_Wtime();
         dt = t1-t0;
-        printf("SMP_Bcast: %lf seconds, %lf MB/s \n", dt, n*(1.e-6/dt));
+        printf("%d: SMP_Bcast: %lf seconds, %lf MB/s \n", wrank, dt, n*(1.e-6/dt));
+        fflush(stdout);
     }
 
     MPI_Free_mem(&buf2);
